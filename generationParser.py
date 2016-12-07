c1eeef887909724b4b569f8cae916154b5df092d
Cobbleopolis <cobbleopolis@gmail.com>
2016-12-06 21:54:52 -0500

    Got all the links for pokemon

--- generationParser.py
+++ generationParser.py
@@ -3,30 +3,43 @@ import bs4
 
 from pokemon import Pokemon
 
-_listUrl = "http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+_base_url = "http://bulbapedia.bulbagarden.net/"
+
+_listUrl = _base_url + "/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
 _res = requests.get(_listUrl)
 _res.raise_for_status()
-soup = bs4.BeautifulSoup(_res.text, "html.parser")
+listSoup = bs4.BeautifulSoup(_res.text, "html.parser")
 
 
 def get_generations():
-    return [header for header in soup.select("h3") if header.text.startswith("Generation")]
+    return [header for header in listSoup.select("h3") if header.text.startswith("Generation")]
 
 
 def get_generation_tables():
     return [header.next_sibling.next_sibling for header in get_generations()]
 
 
-def get_row_entries(gen_number):
-    return [row.find_all() for row in get_generation_tables()[gen_number].find_all('tr')[1:]]
+def get_pokemon_links_from_gen_number(gen_number):
+    return get_pokemon_links_from_gen_table(get_generation_tables()[gen_number])
 
 
-def get_pokemon_from_row_entry(row_entry):
-    # for i in row_entry:
-    #     print(i)
-    print(row_entry)
-    local_number = int(row_entry[0].text.strip()[1:])
-    nat_number = -int(row_entry[1].text.strip()[1:])
+def get_pokemon_links_from_gen_table(gen_table):
+    return [list(row.find_all())[3]['href'] for row in gen_table.find_all('tr')[1:]]
+
+
+def get_all_pokemon_links():
+    out = []
+    for gen_table in get_generation_tables():
+        out += get_pokemon_links_from_gen_table(gen_table)
+    seen = set()
+    seen_add = seen.add
+    return [link for link in out if not (link in seen or seen_add(link))]
+
+
+
+def get_pokemon_from_link(link):
+    local_number = -1
+    nat_number = -1
     name = ""
     types = []
     return Pokemon(local_number, nat_number, name, types)
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pokemon import Pokemon
 
 # for table in generationParser.soup.select("table"):
 #     print(str(table))
-rows = generationParser.get_row_entries(0)
-p = generationParser.get_pokemon_from_row_entry(rows[0])
-print(p)
+links = generationParser.get_all_pokemon_links()
+for l in links:
+    print(l)
+print(len(links))
 # for row in generationParser.get_row_entries(0):
 #     p = generationParser.get_pokemon_from_row_entry(row)
 #     print(p)
